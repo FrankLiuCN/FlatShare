f9f1b8708ac808734e91efa6eb72552a378dbd23
FrankLiuCN <526267542@qq.com>
2015-11-26 23:58:35 +0800

    Signed-off-by: FrankLiuCN <526267542@qq.com>

---
+++ FlatShare.Web/app/controller/PayItemController.js
@@ -0,0 +1,9 @@
+﻿app.controller('payItemController', ['$scope', 'payItemService', function ($scope, payItemService) {
+    $scope.PayItems = [];
+    payItemService.getPayItems().then(function (results) {
+        $scope.PayItems = results;
+    }, function (error) {
+        alert(error);
+    });
+
+}]);
---
+++ FlatShare.Web/app/services/PayItemService.js
@@ -0,0 +1,11 @@
+﻿app.factory('payItemService', ['$http', function ($http) {
+    var serviceBase = 'api/PayItem/GetPayItem';
+    var _getPayItems = function () {
+        return $http.get(serviceBase).then(function (results) {
+            return results;
+        });
+    };
+
+    payItemServiceFactory.getPayItems = _getPayItems;
+    return payItemServiceFactory;
+}]);
